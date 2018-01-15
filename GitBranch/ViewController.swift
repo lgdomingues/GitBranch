ca71e8d3d8b3214e7733b053405cb920eb9b322c
Leandro Guasco Domingues <le.gdomingues@gmail.com>
2018-01-15 21:15:45 -0200

    Implementação da função mensagem()

--- GitBranch/ViewController.swift
+++ GitBranch/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelMensagem: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Exibir a mensagem na Label
+        self.labelMensagem.text = self.mensagem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -19,6 +24,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func mensagem() -> String {
+        
+        // Resgatar a hora atual
+        let agora = Date()
+        
+        // Criar datas para comparação
+        guard let meioDia = Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: agora) else { return "" }
+        guard let seisHoras = Calendar.current.date(bySettingHour: 18, minute: 0, second: 0, of: agora) else { return "" }
+        
+        // Comparação para definir a mensagem
+        if agora >= meioDia && agora <= seisHoras {
+            return "Boa tarde."
+        } else if agora >= seisHoras { 
+            return "Boa noite."
+        } else {
+            return "Bom dia."
+        }
+        
+    }
 
 }
 
